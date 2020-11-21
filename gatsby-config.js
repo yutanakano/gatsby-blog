4cd49b2e931a2c008a741814a7406a14a04eca7a
yutanakano <yutanakano395@icloud.com>
2020-11-21 10:38:46 +0900

    :wrench: siteMetadataを更新

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `生存戦略しましょうか`,
+    author: `yutanakano_net`,
+    description: `全人類同時接続数No1のバトルロワイヤル人生ゲームを生き残れ!`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/yutanakano_net`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/yutanakano`,
       },
     ],
   },

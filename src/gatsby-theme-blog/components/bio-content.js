60d5c8a661e136ea6a68d0c04023a7652c7ef96b
yutanakano <yutanakano395@icloud.com>
2020-12-19 15:01:07 +0900

    :construction: fortawesomeを追加
    npm i --save @fortawesome/react-fontawesome @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/free-brands-svg-icons @fortawesome/free-regular-svg-icons

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+
 export default ({ author, description }) => (
   <>
     {author}<br />
@@ -6,5 +10,19 @@ export default ({ author, description }) => (
     関心事は 業務改善 組織改善 DDD Scrum です。<br />
     <b>{description}</b><br />
     <br />
+    <a href="https://github.com/yutanakano">
+      <FontAwesomeIcon
+        color="#333"
+        size="2x"
+        icon={faGithubSquare} />
+    </a>
+
+    <a href="https://twitter.com/yutanakano_net">
+      <FontAwesomeIcon
+        color="#3eaded"
+        size="2x"
+        icon={faTwitterSquare} />
+    </a>
+
   </>
 );

fbe566c71cd0b1203a837d7af9a4318c1e91b394
yutanakano <yutanakano395@icloud.com>
2020-12-28 23:11:27 +0900

    :lipstick: 文言を追加

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -5,18 +5,23 @@ import {
     FacebookIcon,
     TwitterShareButton,
     TwitterIcon
-  } from 'react-share';
+} from 'react-share';
 
 export default ({
     href,
     title
 }) => (
-  <div className="flex justify-center">
-    <FacebookShareButton url={href}>
-      <FacebookIcon size={64} round />
-    </FacebookShareButton>
-    <TwitterShareButton title={title} via={ 'yutanakano_net' } url={href} >
-      <TwitterIcon size={64} round />
-    </TwitterShareButton> 
-  </div>   
+  <>
+    <div className="flex justify-center p-4">
+        <p className="font-semibold text-gray-500 text-opacity-100">＼ Share ／</p>
+    </div>
+    <div className="flex justify-center">
+        <FacebookShareButton url={href}>
+        <FacebookIcon size={64} round />
+        </FacebookShareButton>
+        <TwitterShareButton title={title} via={ 'yutanakano_net' } url={href} >
+        <TwitterIcon size={64} round />
+        </TwitterShareButton>
+    </div>
+  </>
 );

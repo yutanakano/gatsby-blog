9ec27c811d1318217daf3cc085e983c3de3d13d4
yutanakano <yutanakano395@icloud.com>
2020-12-30 02:23:14 +0900

    :lipstick: PreviousNextのcomponentを作成

--- src/gatsby-theme-blog/components/post-footer.js
+++ src/gatsby-theme-blog/components/post-footer.js
@@ -1,41 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-import { css, Styled, Flex } from "theme-ui"
 
 import Bio from "../components/bio"
+import PreviousNext from "../../components/PreviousNext"
 
 const PostFooter = ({ previous, next }) => (
-  <footer className='flex justify-center'>
-    <Styled.hr />
+  <footer className='max-w-max'>
     <Bio />
-    <div>
-      {(previous || next) && (
-        <Flex
-          as="ul"
-          css={css({
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          })}
-        >
-          <li>
-            {previous && (
-              <Styled.a as={Link} to={previous.slug} rel="prev">
-                ← {previous.title}
-              </Styled.a>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Styled.a as={Link} to={next.slug} rel="next">
-                {next.title} →
-              </Styled.a>
-            )}
-          </li>
-        </Flex>
-      )}
-    </div>
+    <PreviousNext previous={previous} next={next}/>
   </footer>
 )
 

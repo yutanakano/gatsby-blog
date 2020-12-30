29d1263c105119ecacc4f57e7be887944976217d
yutanakano <yutanakano395@icloud.com>
2020-12-31 05:51:19 +0900

    :lipstick: fontの大きさを調整

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -8,8 +8,12 @@ export default ({ avatar, author, description }) => (
           <Image className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" fixed={avatar} alt={author}/>
         </div>
         <div>
-        <h2 className="flex justify-center mt-6 text-gray-800 text-3xl font-semibold">{description}</h2>
-          <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+          <h2 className="flex justify-center mt-6 text-gray-800 text-l font-semibold">
+            {description}
+          </h2>
+          <p className="flex justify-center mt-2 text-gray-600">
+            <p>中野区在住の中野です。大阪生まれ大阪育ちですが就職のタイミングで上京しました。食いっぱぐれないようにすることをモットーに生存戦略を常に考えている元ネトゲ廃人現フルリモートwebエンジニア。Twitterでメンターとして駆け出しエンジニアたちをサポートしています。最近の関心事はDDDとScrum。いずれ起業することを考えてプロダクトを作成中。このブログでは生存戦略を考える上で試行錯誤した内容を記録していきます。</p>
+          </p>
         </div>
         <div className="flex justify-end mt-4">
           <a href="#" className="text-xl font-medium text-indigo-500">{author}</a>
--- src/gatsby-theme-blog/components/post-title.js
+++ src/gatsby-theme-blog/components/post-title.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-const PostTitle = (props) => <h1 className='text-xl' {...props} />
+const PostTitle = (props) => <h1 className='text-3l font-semibold' {...props} />
 
 export default PostTitle

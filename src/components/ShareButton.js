69eb43f019e22ff61fc32600f846f8aca1752eb3
yutanakano <yutanakano395@icloud.com>
2021-01-02 08:21:13 +0900

    :recycle: urlを生成する方法に変更

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import {
   FacebookShareButton,
@@ -7,21 +8,36 @@ import {
   TwitterIcon
 } from 'react-share';
 
-export default ({
-  location,
-  title
-}) => (
-  <>
-    <div className="flex justify-center p-4">
-        <p className="font-semibold text-gray-500 text-opacity-100">＼ Share ／</p>
-    </div>
-    <div className="flex justify-center">
-        <FacebookShareButton url={location.href}>
-          <FacebookIcon size={64} round />
-        </FacebookShareButton>
-        <TwitterShareButton title={title} via={ 'yutanakano_net' } url={location.href} >
-          <TwitterIcon size={64} round />
-        </TwitterShareButton>
-    </div>
-  </>
-);
+const ShareButton = ({title, slug}) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            siteUrl
+          }
+        }
+      }
+    `
+  );
+  const url = (slug) => {
+    return site.siteMetadata.siteUrl + slug
+  }
+  return (
+    <>
+      <div className="flex justify-center p-4">
+          <p className="font-semibold text-gray-500 text-opacity-100">＼ Share ／</p>
+      </div>
+      <div className="flex justify-center">
+          <FacebookShareButton url={url(slug)}>
+            <FacebookIcon size={64} round />
+          </FacebookShareButton>
+          <TwitterShareButton title={title} via={ 'yutanakano_net' } url={url(slug)}>
+            <TwitterIcon size={64} round />
+          </TwitterShareButton>
+      </div>
+    </>
+  );
+};
+
+export default ShareButton;

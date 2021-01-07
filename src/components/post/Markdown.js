ef96a18dfc9401f70fb1ccc2de6457a167db8580
yutanakano <yutanakano395@icloud.com>
2021-01-08 02:33:34 +0900

    :fire: 不要なクラスを削除

--- src/components/post/Markdown.js
+++ src/components/post/Markdown.js
@@ -6,7 +6,7 @@ const Markdown = {
     h3: props => <h3 className='xl font-semibold' {...props} />,
     h4: props => <h4 className='lg font-semibold' {...props} />,
     ul: props => <ul className='list-disc list-inside' {...props} />,
-    li: props => <li className='list-disc list-inside pl-4' {...props} />
+    li: props => <li className='pl-4' {...props} />
 }
 
 export default Markdown

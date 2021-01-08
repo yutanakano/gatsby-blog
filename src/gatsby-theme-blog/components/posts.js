662784cc0a2f03a73080e28b95282713e8f1f8bd
yutanakano <yutanakano395@icloud.com>
2021-01-09 00:09:38 +0900

    :lipstick: TwitterCardにアイコンが表示されるようにした

--- src/gatsby-theme-blog/components/posts.js
+++ src/gatsby-theme-blog/components/posts.js
@@ -3,10 +3,11 @@ import React from "react"
 import Layout from "./../../../node_modules/gatsby-theme-blog/src/components/layout"
 import SEO from "./../../../node_modules/gatsby-theme-blog/src/components/seo"
 import PostList from "./../../../node_modules/gatsby-theme-blog/src/components/post-list"
+import TwitterCard from "../../../content/assets/icon.png"
 
 const Posts = ({ location, posts, siteTitle, socialLinks }) => (
   <Layout location={location} title={siteTitle}>
-    <SEO title="Home" />
+    <SEO title="Home" imageSource={TwitterCard} />
     <main className='container mx-auto flex-grow'>
       <PostList posts={posts} />
     </main>

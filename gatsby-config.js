d3cc58450f08d16f651bb3cb6a63c8c84208215a
yutanakano <yutanakano395@icloud.com>
2021-01-14 01:53:31 +0900

    :heavy_minus_sign: gatsby-plugin-google-analyticsを削除
    npm rm gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,6 @@ module.exports = {
     },
     `gatsby-plugin-postcss`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.

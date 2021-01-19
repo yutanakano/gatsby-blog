94a6b8591861b053ddefb04ee4c62455ae1edfcf
yutanakano <yutanakano395@icloud.com>
2021-01-19 13:10:19 +0900

    :art: Queryを変更できるようにシャドーイングした

---
+++ src/gatsby-theme-blog-core/templates/posts-query.js
@@ -0,0 +1,28 @@
+import { graphql } from "gatsby"
+import PostsPage from "../../../node_modules/gatsby-theme-blog/src/gatsby-theme-blog-core/components/posts"
+
+export default PostsPage
+
+export const query = graphql`
+  query CustomPostsQuery {
+    site {
+      siteMetadata {
+        title
+        social {
+          name
+          url
+        }
+      }
+    }
+    allBlogPost(sort: { fields: [date, title], order: DESC }, limit: 1000) {
+      nodes {
+        id
+        excerpt
+        slug
+        title
+        date(formatString: "MMMM DD, YYYY")
+        tags
+      }
+    }
+  }
+`

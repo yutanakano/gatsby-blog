1b28f0d4ffcef5732d7b597a2f5337f4de16cfe8
yutanakano <yutanakano395@icloud.com>
2021-01-28 22:28:45 +0900

    :sparkles: TagListComponentを作成

---
+++ src/components/TagList.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link, StaticQuery, graphql } from "gatsby"
+
+const TagList = () => (
+  <StaticQuery
+    query = {
+      graphql`
+        query {
+          allMdx(limit: 2000) {
+            group(field: frontmatter___tag) {
+              fieldValue
+              totalCount
+            }
+          }
+        }
+      `
+    }
+    render={data => (
+      <nav>
+        <h1>タグ一覧</h1>
+        <ul>
+          {data.allMdx.group.map(tag => (
+            <li key={tag.fieldValue}>
+              <Link to={`/tags/${tag.fieldValue}/`}>
+                {tag.fieldValue} ({tag.totalCount})
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    )}
+  />
+)
+
+export default TagList

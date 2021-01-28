d440559f3857b2c98f4a870d72450ac5ae2f5eaa
yutanakano <yutanakano395@icloud.com>
2021-01-28 22:29:49 +0900

    :sparkles: TagPageのtemplateを作成

---
+++ src/templates/Tag.js
@@ -0,0 +1,59 @@
+import React from "react"
+
+import { graphql, Link } from "gatsby";
+import Layout from "../gatsby-theme-blog/components/layout"
+import SEO from "../gatsby-theme-blog/components/seo"
+import TwitterCard from "../../content/assets/icon.png"
+
+const Tag = ({ location, pageContext, data }) => {
+  const { tag } = pageContext;
+  const { edges } = data.allMdx;
+
+  return (
+    <Layout>
+      <SEO location={location} title={`${tag}の記事一覧`} imageSource={TwitterCard}/>
+      <main className='container mx-auto flex-grow'>
+        {tag}の記事一覧
+        {
+          edges.map((edge) => {
+            return(
+              <div key={`${edge.node.frontmatter.id}`}>
+                <Link to={createLinkPath(edge.node.slug)}>
+                  {edge.node.frontmatter.title}
+                </Link>
+              </div>
+            )
+          })
+        }
+      </main>
+    </Layout>
+  );
+};
+
+const createLinkPath = (slug) => {
+    return '/'+slug;
+}
+
+export const query = graphql`
+query ($tag: [String]) {
+    allMdx(
+      filter: { frontmatter: { tag: { in: $tag } } },
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          id
+          slug
+          frontmatter {
+            title
+            category
+            date(formatString: "YYYY/MM/DD")
+            tag
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Tag;

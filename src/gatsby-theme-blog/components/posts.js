d2a57dd23f4d00dcdad7c46fe29dc9e9c5971561
yutanakano <yutanakano395@icloud.com>
2021-02-08 16:11:36 +0900

    :sparkles: もっと読む機能を作成

--- src/gatsby-theme-blog/components/posts.js
+++ src/gatsby-theme-blog/components/posts.js
@@ -2,14 +2,14 @@ import React from "react"
 
 import Layout from "./layout"
 import SEO from "./seo"
-import PostList from "./post-list"
+import PostList from "components/PostList"
 import TwitterCard from "../../../content/assets/icon.png"
 
 const Posts = ({ location, posts, siteTitle, socialLinks }) => (
   <Layout location={location} title={siteTitle}>
     <SEO title="Home" imageSource={TwitterCard} />
     <main className='container mx-auto flex-grow'>
-      <PostList posts={posts} />
+      <PostList posts={posts}/>
     </main>
   </Layout>
 )

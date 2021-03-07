381525515ce5f552d6af36c25096176dac018217
yutanakano <yutanakano395@icloud.com>
2021-03-07 21:52:39 +0900

    :lipstick: H5もマッピング

--- src/components/post/Layout.js
+++ src/components/post/Layout.js
@@ -5,6 +5,7 @@ const Layout = {
     h2: props => <h2 className='2xl font-semibold my-4' {...props} />,
     h3: props => <h3 className='xl font-semibold my-4' {...props} />,
     h4: props => <h4 className='lg font-semibold my-4' {...props} />,
+    h5: props => <h4 className='base font-semibold my-4' {...props} />,
     ul: props => <ul className='list-disc list-inside' {...props} />,
     li: props => <li className='pl-4' {...props} />
 }

bfb350f790d74a98b8f1f86636c00d09d46b4e1a
yutanakano <yutanakano395@icloud.com>
2021-03-19 07:19:36 +0900

    :recycle: useStaticQueryに変更

--- src/components/universal/TagList.js
+++ src/components/universal/TagList.js
@@ -1,35 +1,34 @@
 import React from "react"
-import { Link, StaticQuery, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
-const TagList = () => (
-  <StaticQuery
-    query = {
-      graphql`
-        query {
-          allMdx(limit: 2000) {
-            group(field: frontmatter___tags) {
-              fieldValue
-              totalCount
-            }
+const TagList = () => {
+  const { allMdx } = useStaticQuery(
+    graphql`
+      query {
+        allMdx(limit: 2000) {
+          group(field: frontmatter___tags) {
+            fieldValue
+            totalCount
           }
         }
-      `
-    }
-    render={data => (
-      <nav>
-        <h1>タグ一覧</h1>
-        <ul>
-          {data.allMdx.group.map(tag => (
-            <li key={tag.fieldValue}>
-              <Link to={`/tags/${tag.fieldValue}/`}>
-                {tag.fieldValue} ({tag.totalCount})
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    )}
-  />
-)
+      }
+    `
+  );
+  return(
+    <nav>
+      <h1>タグ一覧</h1>
+      <ul>
+        {allMdx.group.map(tag => (
+          <li key={tag.fieldValue}>
+            <Link to={`/tags/${tag.fieldValue}/`}>
+              {tag.fieldValue} ({tag.totalCount})
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
 
 export default TagList

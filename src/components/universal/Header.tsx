ac47d8bb785158e9887184f1161d9330686e5c44
mk1484 <flat.yz14@gmail.com>
2021-04-11 22:16:52 +0900

    ヘッダーの修正 (#31)
    Co-authored-by: mk1484 <flat.yz14@gmail.comm>

--- src/components/universal/Header.tsx
+++ src/components/universal/Header.tsx
@@ -1,57 +1,19 @@
 import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import './Header.css';
 
 const Header = () => {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-gray-500 mb-3">
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
               href="/"
             >
               生存戦略しましょうか
             </a>
-            <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <FontAwesomeIcon color="#333" size="2x" icon={faBars} />
-            </button>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
-          >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <li className="nav-item">
-                <a href="https://github.com/yutanakano">
-                  <FontAwesomeIcon
-                    color="#333"
-                    size="2x"
-                    icon={faGithubSquare}
-                  />
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="https://twitter.com/yutanakano_net">
-                  <FontAwesomeIcon
-                    color="#3eaded"
-                    size="2x"
-                    icon={faTwitterSquare}
-                  />
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </nav>
